9dc1468cdfe4e8572818b962f2b145ea38137d9d
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-02-25 17:47:29 +0530

    added modal and modal provider

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { RenameModal } from "@/components/modals/rename_modal";
+import { useEffect, useState } from "react";
+
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  
+  return (
+    <>
+      <RenameModal />
+    </>
+  );
+};
