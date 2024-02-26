9cd2b07cd7d1b25b6f3ccb84b804d2eb14a211f7
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-02-26 13:24:08 +0530

    fixed screen shot issue with home page

--- components/home/heading.tsx
+++ components/home/heading.tsx
@@ -8,7 +8,7 @@ import ReactCodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { useSession } from "next-auth/react";
 import { Social } from "../auth/social";
 import { javascript } from "@codemirror/lang-javascript";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import CodeTitleBar from "../editor/code-title-bar";
 import { Spinner } from "../spinner";
 import HomeOptions from "./home-options";
@@ -21,41 +21,42 @@ Login to get started.
 export const Heading = () => {
   const container = useRef(null);
   const { data: session, status } = useSession();
+  const [originalcontent, setOriginalcontent] = useState(content);
 
   return (
     <div className="max-w-3xl space-y-4 justify-center flex flex-col items-center align-middle">
       <HomeOptions
         container={container}
-        content={content}
+        content={originalcontent}
         id="home"
         isPublic={true}
         language="typescript"
         title="home"
       />
-      <div
-        ref={container}
-        className="p-5 bg-gradient-to-r from-blue-200 via-sky-500 to-blue-400 rounded-lg"
-      >
-        <CodeTitleBar
-          title={"home"}
-          content={content}
-          fileExtension=".tsx"
-          snippetId="zxrectfvgbhknj345678"
-          snipperAuthorId="234576890-cdfhgbjn"
-        />
-        <ReactCodeMirror
-          className={cn(
-            "w-auto min-w-[250px] max-w-[5xl] max-h-[100%] overflow-y-auto text-lg"
-          )}
-          value={`${content}`}
-          extensions={[
-            javascript({ jsx: true, typescript: true }),
-            EditorView.lineWrapping,
-          ]}
-          readOnly={false}
-          theme={"dark"}
-          placeholder="//Enter code snippet here..."
-        />
+      <div ref={container}>
+        <div className="p-10 mb-5 bg-gradient-to-r from-blue-200 via-sky-500 to-blue-400 rounded-lg">
+          <CodeTitleBar
+            title={"home"}
+            content={originalcontent}
+            fileExtension=".tsx"
+            snippetId=""
+            snipperAuthorId=""
+          />
+          <ReactCodeMirror
+            className={cn(
+              "w-auto min-w-[250px] max-w-[5xl] max-h-[100%] overflow-y-auto text-base"
+            )}
+            value={`${content}`}
+            extensions={[
+              javascript({ jsx: true, typescript: true }),
+              EditorView.lineWrapping,
+            ]}
+            readOnly={false}
+            theme={"dark"}
+            placeholder="//Enter code snippet here..."
+            onChange={setOriginalcontent}
+          />
+        </div>
       </div>
       <div>
         {status === "loading" && (
