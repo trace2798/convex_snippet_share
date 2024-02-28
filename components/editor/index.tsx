052c699d73619cab121cf525faecfe177016595d
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-02-28 21:15:02 +0530

    possible fix to button  issue

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -20,6 +20,7 @@ import {
 } from "../ui/hover-card";
 import { Skeleton } from "../ui/skeleton";
 import { Spinner } from "../spinner";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface EditorProps {
   snippet: Snippet;
@@ -28,7 +29,6 @@ interface EditorProps {
 
 const Editor: FC<EditorProps> = ({ snippet, preview }) => {
   const { data } = useSession();
-  // console.log("DATA ===>", data);
   const [originalcontent, setOriginalcontent] = useState(snippet?.notes);
   const [isGenerating, setIsGenerating] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
@@ -36,13 +36,17 @@ const Editor: FC<EditorProps> = ({ snippet, preview }) => {
 
   if (!snippet) <>Loading</>;
   const { mutate, pending } = useApiMutation(api.snippet.updateNote);
-  const handleSendMessage = async (text: string, language: string) => {
+  const handleSendMessage = async (
+    text: string,
+    language: string,
+    userId: string
+  ) => {
     setIsGenerating(true);
     await sendMessage({
       content: text,
       snippetId: snippet?._id,
       language: language,
-      userId: snippet?.userId,
+      userId: userId as Id<"users">,
     });
     setIsGenerating(false);
   };
@@ -96,6 +100,7 @@ const Editor: FC<EditorProps> = ({ snippet, preview }) => {
                   handleSendMessage(
                     snippet?.content ?? "Content",
                     snippet?.language ?? "typescript",
+                    data?.user.id as Id<"users">
                   )
                 }
                 className="font-medium mt-5 hover:text-indigo-400"
@@ -116,14 +121,6 @@ const Editor: FC<EditorProps> = ({ snippet, preview }) => {
 
         {snippet && !preview && (
           <div className="mt-5 w-full flex justify-center items-center flex-col">
-            {/* <Textarea
-              disabled={isSaving || pending}
-              className="max-w-3xl min-h-[300px] backdrop-blur-sm bg-inherit border-none w-[90%] focus:outline-none focus:ring-0 focus:border-none focus:shadow-none scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
-              value={originalcontent}
-              onChange={(event) => handleNoteChange(event.target.value)}
-            >
-              {isGenerating ? <Skeleton className="h-14 w-[50%]" /> : snippet?.notes}
-            </Textarea> */}
             {isGenerating ? (
               <Skeleton className="h-14 w-[50%] bg-gradient-to-r from-zinc-950 to-zinc-900 via-indigo-900" />
             ) : (
