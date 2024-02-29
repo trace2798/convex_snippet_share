09198e107050c376fff39dfc782a582ee81e035e
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-02-29 12:51:53 +0530

    fix to og

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
-
 export const metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
@@ -18,12 +17,13 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    ogimage: siteConfig.ogImage,
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/og.png`],
+    images: [`${siteConfig.url}og.png`],
     creator: siteConfig.twitterUsername,
   },
   icons: {
